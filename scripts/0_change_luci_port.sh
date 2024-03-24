cd3d9df485ae95f5ff5bab333d5c2bd3873f0152
Neko <nekomancer834@gmail.com>
2024-03-24 03:33:46 -0500

    a

---
+++ scripts/0_change_luci_port.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "change luci port to 81 to avoid conflict with fluidd / mainsail"
+read -p "Press [ENTER] to continue...or [ctrl+c] to exit"
+uci del uhttpd.main.listen_http
+uci add_list uhttpd.main.listen_http='0.0.0.0:81'
+uci add_list uhttpd.main.listen_http='[::]:81'
